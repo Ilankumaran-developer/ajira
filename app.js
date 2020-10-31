3fcacab28e76503db4b4f3d2fcf46c6279b844cf
Ilankumaran-developer <31529469+Ilankumaran-developer@users.noreply.github.com>
2020-10-31 12:15:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path'),
+bodyParser = require('body-parser'),
+http = require('http'),
+RouteHandler = require('./lib/routes');
+let app = null, routes  = null;
+
+class AppLoader {
+    constructor(){
+
+        this.dependencies = {}
+        this.loadApp()
+    }
+    loadApp(){
+        app = express()
+        app.use(bodyParser.text({type: "*/*"}))
+        this.dependencies.app = app;
+        routes = RouteHandler(this.dependencies)
+        app.post('/ajiranet/process', (req,res, next)=>{routes.network.process(req, res, next)})
+        app.get('/hello', (req,res)=>{
+            res.send('Hello world')
+        })
+        let server = http.createServer(app)
+        server.listen('8000');
+    }
+}
+
+module.exports = AppLoader
